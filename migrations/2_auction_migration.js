9a241c4a1a24bfeb9fbb1740155687d1fd4e45c5
Aoi <ht.so.ik0102@gmail.com>
2021-07-07 21:42:31 +0700

    modify migration

--- migrations/2_auction_migration.js
+++ migrations/2_auction_migration.js
@@ -1,5 +1,6 @@
 const Auction = artifacts.require("Auction");
 const ArtToken = artifacts.require("ArtToken");
+const Crowdfunding = artifacts.require("Crowdfunding");
 
 const auctionSetting = {
     biddingTime: Date.now(),
@@ -10,8 +11,6 @@ module.exports = async function (deployer, network, account) {
     deployer.deploy(Auction);
 
     await deployer.deploy(ArtToken, "Art NFT", "ANFT");
-    let tokenInstance = await ArtToken.deployed();
-    await tokenInstance.mint("http:ipfs..");
-    let art = await tokenInstance.getTokenDetail(0);
-    console.log(art);
+    
+    deployer.deploy(Crowdfunding);
 };
